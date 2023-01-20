c3bbd8cfd93f102c8f91a28e0e55f4b3905a513f
Harris Kirk <cjtkirk@protonmail.com>
2023-01-20 15:31:30 -0500

    feat: Obtain and show kubeconfig from cluster

--- gwa-deploy/manage.py
+++ gwa-deploy/manage.py
@@ -4,21 +4,34 @@ Functions that provision the linode instances and volumes
 from common import execute_cli, execute_sh
 import logging
 import time
+import base64
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-9s %(funcName)-30s() %(message)s ",
-    level=logging.INFO,
+    level=logging.DEBUG,
     datefmt="%Y-%m-%d at %H:%M:%S",
 )
 
 KUBERNETES_VERSION = "1.25"
+
+def get_kubeconfig(cluster_id):
+    cmd = [
+        "linode-cli",
+        "--json",
+        "lke",
+        "kubeconfig-view",
+        str(cluster_id),
+    ]
+    json_object = execute_cli(cmd)
+    base_64_kubeconfig = json_object[0]["kubeconfig"]
+    logging.debug(f"kubeconfig base64: {base_64_kubeconfig}")
+    return base64.b64decode(base_64_kubeconfig).decode('ascii')
 
 def configure_cluster():
     # Verify kubectl is installed
     cmd = [
         "kubectl"
     ]
-    print(execute_sh(cmd))
     return
 
 def create_cluster():
@@ -59,8 +72,10 @@ if __name__ == "__main__":
     cluster_id = create_cluster() 
     logging.info(f"Cluster id '{cluster_id}' was created")
     logging.info(f"Sleeping for the nodes to enter Running state (see GH-19)...")
+    time.sleep(300)
+    kubeconfig = get_kubeconfig(cluster_id)
+    logging.debug(f"kubeconfig as yaml: {kubeconfig}")
     configure_cluster()
-    time.sleep(180)
     delete_cluster(cluster_id)
     logging.info(f"Cluster id '{cluster_id}' was deleted")   
 
