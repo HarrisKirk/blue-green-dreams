09dd62033c25b8253c338287203647c280b98ad1
Harris Kirk <cjtkirk@protonmail.com>
2023-03-15 21:22:24 -0400

    feat: Introduce making of REST call to weather API

--- gwa/app.py
+++ gwa/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import datetime
 import os
+import requests
 
 
 def create_app():
@@ -16,15 +17,19 @@ def create_app():
         """
         :return: Flask response
         """
+        weather_api_key = os.environ.get('WEATHER_API_TOKEN')
+        weather_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/richmond%2C%20va?unitGroup=metric&include=days&key={weather_api_key}&contentType=json"
+        json_string = requests.get(weather_url).text
+
         return f"""
             <!DOCTYPE html>
             <html>
             <body>
 
             <h1>It is now {datetime.datetime.now()}</h1>
-
             <p>And the weather is fine in Richmond</p>
-            <p>And the api key is '{os.environ.get('WEATHER_API_TOKEN')[0:5]}'</p>
+            <p>And the api key is '{weather_api_key[0:5]}'</p>
+            <p>The json string is {type(json_string)}</p>
             </body>
             </html>
 
