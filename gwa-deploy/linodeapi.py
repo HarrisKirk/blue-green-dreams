72d5ff5aff87ae9aa23f5d00b5a45f4e1f946851
Harris Kirk <cjtkirk@protonmail.com>
2023-04-29 18:15:48 -0400

    fix: Correct the final API query of linode count.  Closes GH-61

--- gwa-deploy/linodeapi.py
+++ gwa-deploy/linodeapi.py
@@ -6,12 +6,13 @@ import logging
 def get_account_info(tag):
     token = os.environ.get("LINODE_CLI_TOKEN")  
     headers =  {"Content-Type":"application/json", "Authorization": f"Bearer {token}"}
-    response = requests.get(f"https://api.linode.com/v4/tags/{tag}", headers=headers)
+    url = f"https://api.linode.com/v4/linode/instances"
+    logging.debug (f"REST call to {url}")
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         parsed_json = json.loads(response.text)
         pretty_json_string = json.dumps(parsed_json, indent=4, sort_keys=True)
         logging.debug(pretty_json_string)
-        object_count = len(parsed_json["data"])
-        return(object_count)
+        return parsed_json["data"]
     else:
         return(f"Error: {response.status_code} - {response.text}")
