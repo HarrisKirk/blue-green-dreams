71c9e2f78af09babb21fc58cdfc364e6eba2d652
Harris Kirk <cjtkirk@protonmail.com>
2023-05-31 20:37:03 -0400

    refactor: reduce duplicate code in linodeapi module

--- gwa-deploy/linodeapi.py
+++ gwa-deploy/linodeapi.py
@@ -4,13 +4,12 @@ import json
 import logging
 import util
 
-token = os.environ.get("LINODE_CLI_TOKEN")
-headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
-
+TOKEN = os.environ.get("LINODE_CLI_TOKEN")
+HEADERS = {"Content-Type": "application/json", "Authorization": f"Bearer {TOKEN}"}
+LINODE_API_ROOT = "https://api.linode.com/v4"
 
 def get_k8s_nodes():
-    url = f"https://api.linode.com/v4/linode/instances"
-    parsed_json = _invoke_rest_call(url)
+    parsed_json = _invoke_rest_call(f"/linode/instances")
     all_linodes = parsed_json["data"]
     label_prefix_lke = "lke"
     k8s_nodes = [linode for linode in all_linodes if linode["label"].startswith(label_prefix_lke)]
@@ -18,16 +17,14 @@ def get_k8s_nodes():
 
 
 def get_all_clusters():
-    url = f"https://api.linode.com/v4/lke/clusters"
-    parsed_json = _invoke_rest_call(url)
+    parsed_json = _invoke_rest_call(f"/lke/clusters")
     data = parsed_json["data"]
     clusters = [{"id": cluster["id"], "tags": cluster["tags"]} for cluster in data]
     return clusters
 
 
 def get_nodebalancer_id_by_ingress(ingress_ip):
-    url = f"https://api.linode.com/v4/nodebalancers"
-    parsed_json = _invoke_rest_call(url)
+    parsed_json = _invoke_rest_call(f"/nodebalancers")
     logging.debug(parsed_json)
     data = parsed_json["data"]
     ids = [{"id": nb["id"], "ipv4": nb["ipv4"]} for nb in data if nb["ipv4"] == ingress_ip]
@@ -37,8 +34,7 @@ def get_nodebalancer_id_by_ingress(ingress_ip):
 
 
 def get_nodebalancer_id(project: str, env: str):
-    url = f"https://api.linode.com/v4/nodebalancers"
-    parsed_json = _invoke_rest_call(url)
+    parsed_json = _invoke_rest_call(f"/nodebalancers")
     data = parsed_json["data"]
     nodebalancers = []
     for nb in data:
@@ -56,8 +52,7 @@ def get_nodebalancer_id(project: str, env: str):
 
 def get_cluster_id(project: str, env: str):
     """Get the cluster ID of the k8s cluster in the project and environment"""
-    url = f"https://api.linode.com/v4/lke/clusters"
-    parsed_json = _invoke_rest_call(url)
+    parsed_json = _invoke_rest_call(f"/lke/clusters")
     data = parsed_json["data"]
     clusters = []
     for cluster in data:
@@ -75,7 +70,7 @@ def get_cluster_id(project: str, env: str):
 
 def _invoke_rest_call(url: str):
     logging.debug(f"REST call to {url}")
-    response = requests.get(url, headers=headers)
+    response = requests.get(LINODE_API_ROOT + url, headers = HEADERS)
     if response.status_code != 200:
         return f"Error: {response.status_code} - {response.text}"
     parsed_json = json.loads(response.text)
