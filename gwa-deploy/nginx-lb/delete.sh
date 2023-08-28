9aac487e04657f2a3a646e4c97cff85840b3974b
Chris S <chris@soundshare.co.uk>
2023-08-28 23:30:29 +0100

    Fix incorrect linode name

--- gwa-deploy/nginx-lb/delete.sh
+++ gwa-deploy/nginx-lb/delete.sh
@@ -3,7 +3,7 @@ set -x
 set -o pipefail
 set -e
 
-linodeId=$(linode-cli linodes list --label linode-blue-green-lb-2 --json | jq -r '.[0].id')
+linodeId=$(linode-cli linodes list --label linode-blue-green-lb --json | jq -r '.[0].id')
 
 linode-cli linodes delete "$linodeId"
 
