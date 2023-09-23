80f881003fdb6692370093839f672204d3b76d73
Harris Kirk <cjtkirk@protonmail.com>
2023-09-23 12:34:14 -0400

    refactor: transfer switch creation from sh to py * first pass at create
    * feat: ssh to remote box is working
    
    * the apt get install works - just need to refactor
    
    * feat: repeated run works OK
    
    * added smoke test
    
    * final testing OK

--- gwa-deploy/switch.py
+++ gwa-deploy/switch.py
@@ -3,6 +3,8 @@ from retry import retry
 import logging
 import json
 import requests
+import os
+import base64
 
 """
 Issue linode-cli commands to manage the controller switch
@@ -10,46 +12,77 @@ Issue linode-cli commands to manage the controller switch
 
 PROJECT_ACRONYM = "bgd"
 
+
 def switch_delete():
-    cmd = [
-        "linode-cli",
-        "linodes",
-        "list",
-        "--label",
-        "linode-blue-green-lb",
-        "--json"
-    ]
+    cmd = ["linode-cli", "linodes", "list", "--label", "linode-blue-green-lb", "--json"]
     json_object = execute_linode_cli(cmd)
     if json_object == []:
         logging.warning(f"No switch found")
         return
     id = json_object[0]["id"]
     logging.debug(f"switch id: {id}")
-    cmd = [
-        "linode-cli",
-        "linodes",
-        "delete",
-        f"{id}"
-    ]
+    cmd = ["linode-cli", "linodes", "delete", f"{id}"]
     execute_sh(cmd)
     logging.info(f"Deleted linode id {id} with nginx load balancer")
 
 
 def switch_create():
-    cmd = ["bash", "-c", "/gwa_deploy/nginx-lb/setup.sh"]
-    execute_sh(cmd)
-
+    """
+    Create a linode instance with nginx as a switch to route traffic to k8s cluster
+    """
+    cmd = [
+        "linode-cli",
+        "linodes",
+        "create",
+        "--region",
+        "us-east",
+        "--image",
+        "linode/debian11",
+        "--label",
+        "linode-blue-green-lb",
+        "--type",
+        "g6-standard-1",
+        "--authorized_keys",
+        os.environ.get("SSH_NGINX_LB_PUBLIC_KEY"),
+        "--root_pass",
+        os.environ.get("NGINX_LB_ROOT_PASSWORD"),
+    ]
+    json_object = execute_linode_cli(cmd)
+    id = json_object[0]["id"]
+    ip = json_object[0]["ipv4"][0]
+    logging.info(f"Linode instance with id: {id} and IP: {ip} is provisioning")
+    decoded_private_key = base64.b64decode(os.environ.get("SSH_NGINX_LB_PRIVATE_KEY_B64"))
+    private_key_file = "/tmp/bgd_decoded.txt"
+    with open(private_key_file, mode="w") as file:
+        file.write(decoded_private_key.decode())
+    execute_sh(["chmod", "600", private_key_file])
+    cmd = [
+        "ssh",
+        "-o",
+        "StrictHostKeyChecking=no",
+        "-o",
+        "BatchMode=yes",
+        "-i",
+        private_key_file,
+        f"root@{ip}",
+        "hostname",
+    ]
+    wait_for_cmd(cmd)
+    logging.debug(f"switch at ip '{ip}' is in the 'Running' state")
+    cmd = ["ssh", "-i", private_key_file, f"root@{ip}", "apt update && apt install -y nginx"]
+    wait_for_cmd(cmd)
+    os.remove(private_key_file)
+    logging.debug("nginx was installed with 'apt update'")
     logging.info(f"Created linode with nginx load balancer configured for project {PROJECT_ACRONYM}")
-
-    switch_resource = switch_get()
-    if switch_resource != []:
-        if switch_smoke_test(switch_resource[0]["ipv4"][0]):
-            logging.info("[OK] Nginx switch smoke test passes")
-            return switch_view()
-        else:
-            raise Exception(f"Smoke test failed {PROJECT_ACRONYM}")
+    if switch_smoke_test(ip):
+        logging.info(f"[OK] Nginx switch smoke test passes")
     else:
-        logging.info("No switch exists")
+        raise Exception(f"Smoke test of ip {ip} failed")
+
+
+@retry(tries=30, delay=10, logger=logging.getLogger())
+def wait_for_cmd(cmd):
+    execute_sh(cmd)
 
 
 @retry(tries=20, delay=10)
