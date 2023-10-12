4ead78d9a93239d60bc272f3479dc0b92d34e573
Chris S <chris@soundshare.co.uk>
2023-10-12 21:58:59 +0100

    move functions from bgdctl to kubectl

--- gwa-deploy/kubectl.py
+++ gwa-deploy/kubectl.py
@@ -1,8 +1,9 @@
-from common import execute_sh
+from common import execute_sh, execute_linode_cli
 from retry import retry
 import os
 import logging
 import json
+import base64
 
 """
 Issue kubectl commands on the running linode cluster
@@ -89,4 +90,30 @@ def apply_argocd():
 
     # cmd = ["kubectl", "apply", "--namespace=argocd", "--dry-run=server", "-k", "."]
     # output = execute_sh(cmd, "./resources")
+    return
+
+@retry(tries=60, delay=60)
+def get_kubeconfig(cluster_id):
+    cmd = [
+        "linode-cli",
+        "lke",
+        "kubeconfig-view",
+        cluster_id,
+    ]
+    json_object = execute_linode_cli(cmd)
+    base_64_kubeconfig = json_object[0]["kubeconfig"]
+    logging.debug(f"kubeconfig base64: {base_64_kubeconfig}")
+    logging.info(f"kubeconfig received OK from cluster {cluster_id}")
+    return base64.b64decode(base_64_kubeconfig).decode("ascii")
+
+def write_kubeconfig(kubeconfig):
+    KUBECONFIG_DIR = os.environ["HOME"] + "/.kube"
+    KUBECONFIG_FILE_PATH = KUBECONFIG_DIR + "/config"
+    try:
+        os.mkdir(KUBECONFIG_DIR)
+    except FileExistsError:
+        pass
+    with open(KUBECONFIG_FILE_PATH, "w") as file:
+        file.write(kubeconfig)
+    logging.info(f"kubernetes config file written: {KUBECONFIG_FILE_PATH}")
     return
