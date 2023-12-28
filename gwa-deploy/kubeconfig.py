88fb1083ab876c20ef26b426e946edeb1ca4fe06
Harris Kirk <cjtkirk@protonmail.com>
2023-12-28 13:06:03 -0500

    refactor: move kubeconfig code into module

---
+++ gwa-deploy/kubeconfig.py
@@ -0,0 +1,38 @@
+import os
+import base64
+from common import execute_linode_cli, execute_sh
+from retry import retry
+import logging
+
+"""
+Functions to get and set a cluster's kubeconfig file
+"""
+
+def write_kubeconfig(kubeconfig):
+    KUBECONFIG_DIR = os.environ["HOME"] + "/.kube"
+    KUBECONFIG_FILE_PATH = KUBECONFIG_DIR + "/config"
+    try:
+        os.mkdir(KUBECONFIG_DIR)
+    except FileExistsError:
+        pass
+    with open(KUBECONFIG_FILE_PATH, "w") as file:
+        file.write(kubeconfig)
+    logging.info(f"kubernetes config file written: {KUBECONFIG_FILE_PATH}")
+    return
+
+
+@retry(tries=60, delay=60)
+def get_kubeconfig(cluster_id):
+    cmd = [
+        "linode-cli",
+        "lke",
+        "kubeconfig-view",
+        cluster_id,
+    ]
+    json_object = execute_linode_cli(cmd)
+    base_64_kubeconfig = json_object[0]["kubeconfig"]
+    logging.debug(f"kubeconfig base64: {base_64_kubeconfig}")
+    logging.info(f"kubeconfig received OK from cluster {cluster_id}")
+    return base64.b64decode(base_64_kubeconfig).decode("ascii")
+
+
--- gwa-deploy/switch.py
+++ gwa-deploy/switch.py
@@ -1,12 +1,13 @@
 from common import execute_linode_cli, execute_sh
+from kubeconfig import write_kubeconfig, get_kubeconfig
 import linodeapi
 import kubectl
 from retry import retry
 import logging
 import json
+import base64 
 import requests
 import os
-import base64
 import sys
 
 """
@@ -209,7 +210,6 @@ def switch_smoke_test(switch_ip):
         return False
 
 def switch_set_ip_target_to_cluster(env, target_env):
-
     cluster_id = linodeapi.get_cluster_id(PROJECT_ACRONYM, target_env)
 
     if cluster_id is None:
@@ -228,31 +228,3 @@ def switch_set_ip_target_to_cluster(env, target_env):
 
     logging.debug(f"Attempt to set switch target IP to cluster's ingress IP ({ingress_ip})")
     switch_ip_set(env, ingress_ip)
-
-# TODO - these functions are duplicated.  Refactor
-@retry(tries=60, delay=60)
-def get_kubeconfig(cluster_id):
-    cmd = [
-        "linode-cli",
-        "lke",
-        "kubeconfig-view",
-        cluster_id,
-    ]
-    json_object = execute_linode_cli(cmd)
-    base_64_kubeconfig = json_object[0]["kubeconfig"]
-    logging.debug(f"kubeconfig base64: {base_64_kubeconfig}")
-    logging.info(f"kubeconfig received OK from cluster {cluster_id}")
-    return base64.b64decode(base_64_kubeconfig).decode("ascii")
-
-
-def write_kubeconfig(kubeconfig):
-    KUBECONFIG_DIR = os.environ["HOME"] + "/.kube"
-    KUBECONFIG_FILE_PATH = KUBECONFIG_DIR + "/config"
-    try:
-        os.mkdir(KUBECONFIG_DIR)
-    except FileExistsError:
-        pass
-    with open(KUBECONFIG_FILE_PATH, "w") as file:
-        file.write(kubeconfig)
-    logging.info(f"kubernetes config file written: {KUBECONFIG_FILE_PATH}")
-    return
